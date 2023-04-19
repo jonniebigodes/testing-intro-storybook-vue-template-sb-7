d87fbc8024f27747eb79814ca9d3b3d51bcf7503
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-04-19 21:39:59 +0100

    Changes configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,16 @@
+import '../src/index.css'
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
+        date: /Date$/
+      }
+    }
+  }
+}
 
-export default preview;
+export default preview
